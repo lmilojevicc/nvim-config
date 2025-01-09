2e0c9aa475dc1310acd648705e9bbd9058a81c38
Luka <lmilojeviccc@gmail.com>
2025-01-09 21:40:22 +0100

    changed separator style

--- lua/luka/plugins/bufferline.lua
+++ lua/luka/plugins/bufferline.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("bufferline").setup({
       options = {
-        separator_style = { "  ", "  " }, -- Custom separators
+        separator_style = { " | ", " | " }, -- Custom separators
         indicator = {
           style = "none",
         },
