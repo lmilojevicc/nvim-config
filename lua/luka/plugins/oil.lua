14c873e00cbdaf94298c69938ca6a4d97eb0481f
Luka <lmilojeviccc@gmail.com>
2025-03-29 16:18:26 +0100

    added oli nvim

---
+++ lua/luka/plugins/oil.lua
@@ -0,0 +1,23 @@
+return {
+  "stevearc/oil.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  keys = {
+    {
+      "<leader>oi",
+      function()
+        require("oil").toggle_float()
+      end,
+      desc = "Open Floating Oil Nvim",
+    },
+  },
+  opts = {
+    float = {
+      max_width = 0.3,
+      max_height = 0.2,
+    },
+    default_file_explorer = true,
+    view_options = {
+      show_hidden = true,
+    },
+  },
+}
