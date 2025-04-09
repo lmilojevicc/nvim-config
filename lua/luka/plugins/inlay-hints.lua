2b2f2fdcf6f2b967616e50f6bef785e0ed504b10
Luka <lmilojeviccc@gmail.com>
2025-04-09 23:58:19 +0200

    turned off inlay-hints initially

--- lua/luka/plugins/inlay-hints.lua
+++ lua/luka/plugins/inlay-hints.lua
@@ -3,7 +3,10 @@ return {
   event = "LspAttach",
   dependencies = { "neovim/nvim-lspconfig" },
   config = function()
-    require("inlay-hints").setup()
+    require("inlay-hints").setup({
+      autocmd = { enable = false },
+      commands = { enable = true },
+    })
   end,
   keys = {
     { "<leader>gl", "<cmd>InlayHintsToggle<CR>", desc = "󰨚 Toggle Inlay Hints" },
