fc739b5600342c3260b9438471365b6191bad392
Luka <104729643+lmilojevicc@users.noreply.github.com>
2025-12-28 02:58:32 +0100

    Add oil preview

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -4,8 +4,8 @@ return {
 
   opts = {
     float = {
-      max_width = 0.4,
-      max_height = 0.4,
+      max_width = 0.6,
+      max_height = 0.6,
       border = "single",
     },
     ssh = {
@@ -17,6 +17,20 @@ return {
     default_file_explorer = true,
     view_options = { show_hidden = true },
   },
+
+  config = function(_, opts)
+    require("oil").setup(opts)
+
+    vim.api.nvim_create_autocmd("User", {
+      pattern = "OilEnter",
+      callback = vim.schedule_wrap(function(args)
+        local oil = require("oil")
+        if vim.api.nvim_get_current_buf() == args.data.buf and oil.get_cursor_entry() then
+          oil.open_preview()
+        end
+      end),
+    })
+  end,
 
   --stylua: ignore
   keys = {
